bdbcf758b3a94ba3a18524b3c98686af5b980b9b
Anowar Uddin <lenin3636@gmil.com>
2018-10-14 13:39:48 +0200

    footer added to the story

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
 import Calendar from "./components/calendar/Calendar";
-import MapComponent from "./components/map-component/MapComponent"
+// import MapComponent from "./components/map-component/MapComponent"
 import logo from './logo.svg';
 import './App.css';
+import FooterPage from './components/Footer/FooterPage';
+import Dropdown from './components/DropDown/Dropdown';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Header />
+        <Header />
+        <Dropdown />
         <Calendar />
-        <div>
-          <MapComponent />
-        </div>
+        <footer>
+          <FooterPage />
+        </footer>
       </div>
     );
   }
